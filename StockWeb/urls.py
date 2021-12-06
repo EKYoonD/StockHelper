ea82634cc773a7ceb2791f112fb85c0449496561
binigy97 <75118895+binigy97@users.noreply.github.com>
2021-12-06 22:08:25 +0900

    장고 웹 : 서버 세팅, stockList.txt 생성
    서버 세팅 : url 관련 세팅 진행 완료 (서버 연결 시 각 페이지 조회 가능)
    
    stockList.txt : 메인 페이지에서 검색 결과를 담고 있는 문서

--- StockWeb/urls.py
+++ StockWeb/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.find)
+    path('', views.find),
+    path('/analysis', views.search),
 ]
--- StockWeb/views.py
+++ StockWeb/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def find(request):
     return render(request, 'index.html')
+
+def search(request):
+    return render(request, 'analysis.html')
