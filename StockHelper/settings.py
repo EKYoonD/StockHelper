987eb2675d4580d4b546d106dfae5c6bfa679196
Eunkyeong Yoon <90364276+EKYoonD@users.noreply.github.com>
2021-12-06 14:43:01 +0900

    Dir 수정

--- StockHelper/settings.py
+++ StockHelper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^2t)x53mcw5f%-5p6m*jssa$xc+k)cm!^og%&m)c7si^a4lbyy'
+SECRET_KEY = 'django-insecure-22t3a8g94i08_+o2urc=&@0x29&$@j5c7$5dvtr5f-1b4--b34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ StockWeb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StockwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'StockWeb'
