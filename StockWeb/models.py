d17febaa31618fa6a2988f34e4a237fb2e8ab443
binigy97 <75118895+binigy97@users.noreply.github.com>
2021-12-09 20:22:56 +0900

    view : 미완성
    같은 자료 공유하기 위한 push (진행사항 없음)

--- StockWeb/models.py
+++ StockWeb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# class Search(models.Model):
+#     search_input = models.TextField()
+#     result = []
+
+#     @property
+#     def findComCode(self):
+#         with open('dataset/stockList.txt', encoding='utf-8') as txtfile:
+#             for contents in txtfile.readlines():
+#                 content =  contents.split(',')
+#                 com_name = content[0]
+#                 com_code = content[1]
+#                 # print(com_code)
+#                 if self.search_input in com_name:
+#                     self.result.append(com_code)
+#         return self.result
--- StockWeb/views.py
+++ StockWeb/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+# from StockWeb.models import Search
+# from django.db.models import Q
 
 # Create your views here.
 def find(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        # get parameter
+        searchInput = request.POST['search_input']
+        print(searchInput)
+        # match stockList and search_input
+        # seachResult = Search.objects.all().filter(
+        #     Q(name__icontains=searchInput),
+        #     Q(description__icontains=searchInput)
+        # )
+        return render(request, 'index.html')
+    else:
+        print("GET 입니다")
+        return render(request, 'index.html')
 
 def search(request):
     return render(request, 'analysis.html')
