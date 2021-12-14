954ab8fa7ec2deeb9b8ae89d6daff30b8fc6ddef
binigy97 <75118895+binigy97@users.noreply.github.com>
2021-12-14 18:13:07 +0900

    장고 웹 : patentKeyword .js디버깅
    사용 data 변경

--- static/js/patentKeyword.js
+++ static/js/patentKeyword.js
@@ -1,31 +1,20 @@
-function getToday(){
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = ("0" + (1 + date.getMonth())).slice(-2);
-    var day = ("0" + date.getDate()).slice(-2);
-
-    return year + month + day;
-}
-
 $(document).ready(function() {
     // 특허 키워드 순위 top50 보여줌
-    var today = '20211208'
-    // var today = getToday()
     var cnt = 1
 
     for (j of [10, 20, 30, 40, 50]) {
         var res = $('#patent_kw_top' + j);   // res 결과가 들어갈 자리
-        res.load('../static/data/count_patent_' + today + '.csv', function (data) {
+        res.load('../static/data/count_patent_desc.csv', function (data) {
             var lines = data.split('\n');
             var list = '';
             
-            alert(j + " " + cnt + " " + typeof(j))
+            // alert(j + " " + cnt + " " + typeof(j))
             for (i = cnt; i <= j; i++) {
                 var element = lines[i].split(',');
                 list += '<li><a href="/StockWeb/patent">'
-                        + '<span class="rank text-warning mr-1">' + i + '</span>'
-                        + '<span class="kw mr-1">' + element[0] + '</span>'
-                        + '<span class="cnt text-warning">' + element[1] + '</span>'
+                        + '<span class="rank text-warning mr-1">' + element[0] + '</span>'
+                        + '<span class="kw mr-1">' + element[1] + '</span>'
+                        + '<span class="cnt text-warning">' + element[2] + '</span>'
                         + '</a></li>'
             }
             cnt += 10
--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,12 +1,3 @@
-function getToday(){
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = ("0" + (1 + date.getMonth())).slice(-2);
-    var day = ("0" + date.getDate()).slice(-2);
-
-    return year + month + day;
-}
-
 $(document).ready(function() {
     // + 아이콘 클릭시 페이지 이동 
     $('#more_btn').click(function(){
@@ -15,18 +6,16 @@ $(document).ready(function() {
 
     // aside 태그에 특허 키워드 순위 보여줌
     var res = $('#patent_kw');   // res 결과가 들어갈 자리
-    var today = '20211208'
-    // var today = getToday()
-    res.load('../static/data/count_patent_' + today + '.csv', function (data) {
+    res.load('../static/data/count_patent_desc.csv', function (data) {
         var lines = data.split('\n');
         var list = '';
         
         for (i = 1; i <= 10; i++) {
             var element = lines[i].split(',');
             list += '<li><a href="/StockWeb/patent">'
-                        + '<span class="rank text-warning mr-1">' + i + '</span>'
-                        + '<span class="kw mr-1">' + element[0] + '</span>'
-                        + '<span class="cnt text-warning">' + element[1] + '</span>'
+                        + '<span class="rank text-warning mr-1">' + element[0] + '</span>'
+                        + '<span class="kw mr-1">' + element[1] + '</span>'
+                        + '<span class="cnt text-warning">' + element[2] + '</span>'
                         + '</a></li>'
         }
         res.html(list);   // res 결과를 html에 보여줌
