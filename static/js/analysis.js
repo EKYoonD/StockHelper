37a8eb0d2a0c9c35725629ff1cd4e51ab8e3def4
hahbr88 <hahbr88@hanmail.net>
2021-12-17 00:09:25 +0900

    종가 차트 완성 및 불필요한 파일 삭제

--- static/js/analysis.js
+++ static/js/analysis.js
@@ -70,6 +70,7 @@ function all_chart(data_set, stock_name) {
 }
 
 // 종가 그래프 그리기
+
 function close_chart(stock_close_date_list, stock_close_price_list) {
     stock_close_date_list = stock_close_date_list.split(',')
     stock_close_price_list = stock_close_price_list.split(',')
@@ -82,47 +83,62 @@ function close_chart(stock_close_date_list, stock_close_price_list) {
         data: { // 차트에 들어갈 데이터
             labels:
                 //x 축 (리스트) 
-                stock_close_date_list
-                // [1,2,3,4,5,6]
-                // ['1','2','3','4','5','6']
+                stock_close_date_list,
 
-                ,
             datasets: [{ //데이터
-                    label: '최근 20일간의 종가 + 예측 종가', //차트 제목
+                    label: '종가', //차트 제목
                     fill: false, // line 형태일 때, 선 안쪽을 채우는지 안채우는지
                     data: 
-                        stock_close_price_list //x축 label에 대응되는 데이터 값 (list)
-                        // [5,6,7,8,9,10]
+                        stock_close_price_list //x축 label에 대응되는 데이터 값
                         ,
-                    backgroundColor: [
+                    backgroundColor: 
                         //색상
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        //경계선 색상
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
+
+                        '#f0ad4e'
+                    ,
                     borderWidth: 1 //경계선 굵기
                 }
             ]
         },
         options: {
+            title:{
+                display:true,
+                text:'최근 20일간의 종가 + 예측 종가'
+            },
+            legend: {
+                labels: {
+                    fontColor: 'black',
+                    fontSize: 16,
+                }
+            },
             scales: {
+                xAxes: [{
+                    ticks: {
+                        fontColor: 'black',
+                        fontSize: '10',
+                    }
+                }],
                 yAxes: [{
                     ticks: {
-                        beginAtZero: true
+                        beginAtZero: true,
+                        fontColor: 'black',
+                        fontSize: '10',
                     }
                 }]
+            },
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            let label = context.dataset.label || '';
+    
+                            if (label) {
+                                label += ': ';
+                            }
+                            return label;
+                        }
+                    }
+                }
             }
         }
     });
@@ -130,7 +146,7 @@ function close_chart(stock_close_date_list, stock_close_price_list) {
 
 $(document).ready(function(){
     // 종가 상하강 여부에 따른 글자색 변화
-    var upDown = $('#up_down').val();
+    var upDown = $('#up_down').text();
     if (upDown == '상승') {
         $('.stock_upDown').css({"color": "#d9534f", "font-weight": "bold"}) // 빨강(danger)
     } else if (upDown == '하강') {
