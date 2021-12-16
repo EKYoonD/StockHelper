c3dd41b5b7a479a0db253f9c68a16a891847a7d9
binigy97 <75118895+binigy97@users.noreply.github.com>
2021-12-16 16:16:36 +0900

    장고 웹 : analaysis에 기업정보 추가 (미완)

---
+++ static/js/analysisCompany.js
@@ -0,0 +1,49 @@
+// 기업정보에 들어가는 chart
+function chart(data_set, stock_name) {
+    data_set = data_set.split(',')
+    var data = []
+    var buffer = []
+    for(i = 0; i < data_set.length; i++) {
+        if(buffer.length == 5) {
+            data.push(buffer)
+            buffer = []
+        }
+
+        buffer.push(Number(data_set[i]))
+    }
+
+    // create the chart
+    chart = new Highcharts.stockChart('companyChart', {
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: '2020-01-01 이후 ' + stock_name + ' 주식 그래프'
+        },
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        plotOptions: {
+            candlestick: {
+                downColor: 'blue',
+                upColor: 'red'
+            }
+        },
+
+        series: [{
+            type: 'candlestick',
+            name: stock_name,
+            data: data,
+            tooltip: {
+            valueDecimals: 8
+            }
+        }]
+    });
+}
+
+$(document).ready(function() {
+});
